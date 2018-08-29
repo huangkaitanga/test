2d214602682d93e85aeac934e4375ace2d8cd915
huangkaitang <42802014+huangkaitang@users.noreply.github.com>
2018-08-29 20:47:04 +0800

    第一个java类
    这里是我上传的第一个java类，谢谢哈！

---
+++ School.java
@@ -0,0 +1,10 @@
+class Student{
+	public static void main (String [] args){
+		System.out.println("Student!");
+	}
+}
+class Teacher{
+	public static void main (String [] args){
+		System.out.println("Teacher!");
+	}
+}
